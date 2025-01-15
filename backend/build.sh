d8f210eed27d93a1c3a18ee0e5c45d96c51f38ce
takitahmid20 <takitahmid20@gmail.com>
2025-01-16 00:42:49 +0600

    Remove migrations from build script since using raw SQL

--- backend/build.sh
+++ backend/build.sh
@@ -5,8 +5,5 @@ set -o errexit
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Run migrations
-python manage.py migrate
-
 # Collect static files
 python manage.py collectstatic --no-input
